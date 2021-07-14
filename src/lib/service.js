6a5aa3580d111d8035ed50dbae45fe2cc8fde72a
togo-mentor <uint2435@gmail.com>
2021-07-15 06:57:01 +0900

    fix: request to 3000

--- src/lib/service.js
+++ src/lib/service.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export const saveTodo = (todo) => 
-  axios.post('http://localhost:3001/api/todos', todo)
+  axios.post('http://localhost:3000/api/todos', todo)
 
 export const loadTodos = () => 
-  axios.get('http://localhost:3001/api/todos')
+  axios.get('http://localhost:3000/api/todos')
 
 export const destroyTodo = (id) => 
-  axios.delete(`http://localhost:3001/api/todos/${id}`)
+  axios.delete(`http://localhost:3000/api/todos/${id}`)
 
 export const uppdateTodo = (todo) => 
-  axios.put(`http://localhost:3001/api/todos/${todo.id}`, todo)
+  axios.put(`http://localhost:3000/api/todos/${todo.id}`, todo)
