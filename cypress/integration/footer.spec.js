59a93fc1ba3836eb67b59c74242775dde143edaa
togo-mentor <uint2435@gmail.com>
2021-07-14 19:05:58 +0900

    test: with multiple todos

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -4,9 +4,15 @@ describe('Footer', () => {
       cy.seedAndVisit([{id: 1, name: 'buy milk', isComplete: false}])
       cy.get('.todo-count')
       .should('contain', '1 todo left')
-
-      
     });
   })
-  
+
+  context('with multiple todos', () => {
+    it.only('displays plural todos in count', () => {
+      cy.seedAndVisit()
+
+      cy.get('.todo-count')
+      .should('contain', '3 todos left')
+    })
+  })
 })
