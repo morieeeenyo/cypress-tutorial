a0169112c2be245d3ea9abd03f4845652320977b
togo-mentor <uint2435@gmail.com>
2021-07-15 06:18:35 +0900

    test: refactor: filter in array

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -17,20 +17,20 @@ describe('Footer', () => {
       .should('contain', '3 todos left')
     })
 
-    it('Filters to active todos', () => {
-      cy.contains('Active')
-      .click()
+    it('Filters todos', () => {
+      const filters = [
+        {link: 'Active', expectedLength: 3},
+        {link: 'Completed', expectedLength: 1}
+      ]
 
-      cy.get('.todo-list li')
-      .should('have.length', 3)
-    });
-
-    it('Filters to active todos', () => {
-      cy.contains('Completed')
-      .click()
-
-      cy.get('.todo-list li')
-      .should('have.length', 1)
-    });
+      cy.wrap(filters)
+      .each(filter => {
+        cy.contains(filter.link)
+        .click()
+  
+        cy.get('.todo-list li')
+        .should('have.length', filter.expectedLength)
+      })
+    });;
   })
 })
